0818670c790537bf40955f86f13cf41c724a8683
Vano-Stas <st.ivan727@gmail.com>
2019-07-04 22:49:40 +0300

    Updates

---
+++ precache-manifest.7c4d6242bab9e35b202ffc2d0ab98fc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9dcf43cda33963b7c6dd1e6ea469f86",
+    "url": "/swapi-actors/index.html"
+  },
+  {
+    "revision": "802533de7bfecea18e8c",
+    "url": "/swapi-actors/static/css/main.d7c2cf94.chunk.css"
+  },
+  {
+    "revision": "e651e2574ebea393ad5f",
+    "url": "/swapi-actors/static/js/2.a64dfd47.chunk.js"
+  },
+  {
+    "revision": "802533de7bfecea18e8c",
+    "url": "/swapi-actors/static/js/main.48292b74.chunk.js"
+  },
+  {
+    "revision": "df9f8a7ce6bbbe58bd2b",
+    "url": "/swapi-actors/static/js/runtime~main.5ff5c053.js"
+  }
+]);
